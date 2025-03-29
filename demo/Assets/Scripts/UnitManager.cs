4aeef451481dc16df335272837e89169cc045bae
Mattynator0 <mm6205@wp.pl>
2025-03-29 18:27:55 +0100

    Basic unit movement
    Created a UnitManager class for handling unit management logic. UnitManager checks the tag of the chosen tile before issuing a moveTo order to the unit.

---
+++ demo/Assets/Scripts/UnitManager.cs
@@ -0,0 +1,167 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine.WSA;
+
+public class UnitManager : MonoBehaviour
+{
+    public static UnitManager Instance { get; private set; }
+
+    private List<UnitBasic> selectedUnits = new List<UnitBasic>();
+    private UnitBasic selectedUnit;
+
+    private float touchStartTime = 0;
+    private Vector2 touchWorldPosition = Vector2.zero;
+    private Vector2 touchScreenPosition = Vector2.zero;
+    private Vector3Int touchGridPosition = Vector3Int.zero;
+    private Collider2D hitCollider = null;
+
+    public Transform gridParent;
+    private List<Tilemap> tilemaps;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PopulateTilemapsFromGrid();
+
+        TouchManager.Instance.OnTouchBegan += HandleTouchBegan;
+        TouchManager.Instance.OnTouchEnded += HandleTouchEnded;
+    }
+    void PopulateTilemapsFromGrid()
+    {
+        tilemaps = new List<Tilemap>();
+        foreach (Transform child in gridParent)
+        {
+            Tilemap tilemap = child.GetComponent<Tilemap>();
+            if (tilemap != null)
+            {
+                tilemaps.Add(tilemap);
+            }
+        }
+    }
+
+    void HandleTouchBegan(Vector2 touchPosition)
+    {
+        touchStartTime = Time.time;
+        touchWorldPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+        touchScreenPosition = touchPosition;
+
+        GridLayout gridLayout = gridParent.GetComponent<GridLayout>();
+        if (gridLayout != null)
+        {
+            Vector2 cellSize = gridLayout.cellSize;
+
+            // convert world position to grid position
+            Vector3 localPosition = gridParent.InverseTransformPoint(touchWorldPosition);
+            touchGridPosition = new Vector3Int(
+                Mathf.FloorToInt(localPosition.x / cellSize.x),
+                Mathf.FloorToInt(localPosition.y / cellSize.y),
+                0
+            );
+        }
+        else
+        {
+            Debug.LogError("GridLayout component not found on gridParent.");
+        }
+
+        hitCollider = Physics2D.OverlapPoint(touchWorldPosition);
+    }
+
+    void HandleTouchEnded(Vector2 touchEndedPosition)
+    {
+        // check if user tapped, ignore drag and hold gestures
+        if (Vector2.Distance(touchEndedPosition, touchScreenPosition) < 100f && Time.time - touchStartTime < 1f)
+        {
+            if (hitCollider != null)
+            {
+                UnitBasic unit = hitCollider.GetComponent<UnitBasic>();
+                if (unit != null)
+                {
+                    ToggleSelection(unit);
+                }
+            }
+            else if (selectedUnit != null)
+            {
+                Vector2 targetPosition = Camera.main.ScreenToWorldPoint(touchEndedPosition);
+                if (CanMoveTo(touchGridPosition))
+                {
+                    selectedUnit.MoveTo(targetPosition);
+                }
+            }
+        }
+    }
+
+    bool CanMoveTo(Vector3Int gridPosition)
+    {
+        bool canMove = true;
+        Tilemap topTilemap = null;
+        int topTileOrderInLayer = int.MinValue;
+
+        foreach (var tilemap in tilemaps)
+        {
+            TileBase tile = tilemap.GetTile(gridPosition);
+            if (tile != null)
+            {
+                Renderer renderer = tilemap.GetComponent<Renderer>();
+
+                if (renderer != null && renderer.sortingOrder > topTileOrderInLayer)
+                {
+                    topTileOrderInLayer = renderer.sortingOrder;
+                    topTilemap = tilemap;
+                }
+            }
+        }
+
+        if (topTilemap != null)
+        {
+            TileBase tile = topTilemap.GetTile(gridPosition);
+
+            if (tile != null && topTilemap.CompareTag("Obstacle"))
+            {
+                canMove = false;
+            }
+        }
+        return canMove;
+    }
+
+
+    void ToggleSelection(UnitBasic unit)
+    {
+        // deselect if same unit
+        if (selectedUnit == unit)
+        {
+            selectedUnit.Deselect();
+            selectedUnit = null;
+        }
+        // deselect old unit and select new unit
+        else
+        {
+            if (selectedUnit != null)
+            {
+                selectedUnit.Deselect();
+            }
+            selectedUnit = unit;
+            selectedUnit.Select();
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (TouchManager.Instance != null)
+        {
+            TouchManager.Instance.OnTouchBegan -= HandleTouchBegan;
+            TouchManager.Instance.OnTouchEnded -= HandleTouchEnded;
+        }
+    }
+}
--- demo/Assets/Scripts/unit_basic.cs
+++ demo/Assets/Scripts/unit_basic.cs
@@ -11,26 +11,11 @@ public class UnitBasic : MonoBehaviour
     private Vector2 targetPosition;
     private bool moving = false;
 
-    float touchStartTime = 0;
-    Vector2 touchMapPosition = Vector2.zero;
-    Vector2 touchScreenPosition = Vector2.zero;
-    Collider2D hitCollider = null;
-    UnitBasic unitManagerInstance; // to bedzie w unit manager later, teraz jest by pokazzaæ jak dziala przycisk
-
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         defaultColor = spriteRenderer.color;
         targetPosition = transform.position;
-
-        TouchManager.Instance.OnTouchBegan += HandleTouchBegan;
-        TouchManager.Instance.OnTouchEnded += HandleTouchEnded;
-    }
-
-    void Awake()
-    {
-        if (unitManagerInstance == null) unitManagerInstance = this;
-        else Destroy(gameObject);
     }
 
     void Update()
@@ -45,43 +30,22 @@ public class UnitBasic : MonoBehaviour
         }
     }
 
-    void HandleTouchBegan(Vector2 touchPosition)
+    public void Select()
     {
-        touchStartTime = Time.time;
-        touchMapPosition = Camera.main.ScreenToWorldPoint(touchPosition); // what place we clicked on the world 
-        touchScreenPosition = touchPosition; // where we clicked on the screen
-
-        hitCollider = Physics2D.OverlapPoint(touchMapPosition);
+        isSelected = true;
+        spriteRenderer.color = selectedColor;
     }
 
-    void HandleTouchEnded(Vector2 touchPosition)
+    public void Deselect()
     {
-        if (Vector2.Distance(touchPosition, touchScreenPosition) < 100f && Time.time - touchStartTime < 1f) // check if not dragging or holding
-        {
-            if (hitCollider != null && hitCollider.gameObject == gameObject)
-            {
-                ToggleSelection();
-            }
-            else if (isSelected)
-            {
-                targetPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-                moving = true;
-            }
-        }
+        isSelected = false;
+        spriteRenderer.color = defaultColor;
     }
 
-    void ToggleSelection()
+    public void MoveTo(Vector2 position)
     {
-        isSelected = !isSelected;
-        spriteRenderer.color = isSelected ? selectedColor : defaultColor;
-    }
-
-    void OnDestroy()
-    {
-        if (TouchManager.Instance != null)
-        {
-            TouchManager.Instance.OnTouchBegan -= HandleTouchBegan;
-            TouchManager.Instance.OnTouchEnded -= HandleTouchEnded;
-        }
+        // TODO some kind of pathing to avoid obstacles
+        targetPosition = position;
+        moving = true;
     }
 }
