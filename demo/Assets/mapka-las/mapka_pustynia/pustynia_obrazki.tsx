ed120fda7df65ad2e383d579890f2e1302d55b4c
Mattynator0 <mm6205@wp.pl>
2025-06-09 20:14:23 +0200

    Desert map
    Desert map scene with all scripts and collisions added

---
+++ demo/Assets/mapka-las/mapka_pustynia/pustynia_obrazki.tsx
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="pustynia_obrazki" tilewidth="769" tileheight="630" tilecount="63" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="98">
+  <image source="free-2d-rpg-desert-tileset/PNG/building_2.png" width="325" height="189"/>
+ </tile>
+ <tile id="99">
+  <image source="free-2d-rpg-desert-tileset/PNG/bg.png" width="256" height="256"/>
+ </tile>
+ <tile id="100">
+  <image source="free-2d-rpg-desert-tileset/PNG/building_1.png" width="280" height="275"/>
+ </tile>
+ <tile id="101">
+  <image source="free-2d-rpg-desert-tileset/PNG/decor_2.png" width="129" height="140"/>
+ </tile>
+ <tile id="102">
+  <image source="free-2d-rpg-desert-tileset/PNG/decor_3.png" width="57" height="58"/>
+ </tile>
+ <tile id="103">
+  <image source="free-2d-rpg-desert-tileset/PNG/lake.png" width="559" height="221"/>
+ </tile>
+ <tile id="104">
+  <image source="free-2d-rpg-desert-tileset/PNG/tree_5.png" width="187" height="244"/>
+ </tile>
+ <tile id="105">
+  <image source="free-2d-rpg-desert-tileset/PNG/tree_4.png" width="123" height="323"/>
+ </tile>
+ <tile id="106">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_6.png" width="84" height="73"/>
+ </tile>
+ <tile id="107">
+  <image source="free-2d-rpg-desert-tileset/PNG/tree_10.png" width="218" height="172"/>
+ </tile>
+ <tile id="108">
+  <image source="free-2d-rpg-desert-tileset/PNG/tree_7.png" width="224" height="281"/>
+ </tile>
+ <tile id="109">
+  <image source="free-2d-rpg-desert-tileset/PNG/tree_9.png" width="378" height="443"/>
+ </tile>
+ <tile id="110">
+  <image source="free-2d-rpg-desert-tileset/PNG/tree_6.png" width="205" height="255"/>
+ </tile>
+ <tile id="111">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_House/Goblin_House.png" width="128" height="192"/>
+ </tile>
+ <tile id="112">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_House/Goblin_House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="113">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_InConstruction.png" width="256" height="192"/>
+ </tile>
+ <tile id="114">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Destroyed.png" width="256" height="192"/>
+ </tile>
+ <tile id="115">
+  <image source="Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="116">
+  <image source="free-2d-rpg-desert-tileset/PNG/decor_8.png" width="315" height="178"/>
+ </tile>
+ <tile id="117">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_3.png" width="87" height="162"/>
+ </tile>
+ <tile id="118">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_4.png" width="89" height="101"/>
+ </tile>
+ <tile id="119">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_5.png" width="69" height="107"/>
+ </tile>
+ <tile id="120">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_6.png" width="84" height="73"/>
+ </tile>
+ <tile id="121">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_1.png" width="109" height="303"/>
+ </tile>
+ <tile id="122">
+  <image source="free-2d-rpg-desert-tileset/PNG/greenery_2.png" width="55" height="59"/>
+ </tile>
+ <tile id="123">
+  <image source="free-2d-rpg-desert-tileset/PNG/land_8.png" width="256" height="256"/>
+ </tile>
+ <tile id="124">
+  <image source="free-2d-rpg-desert-tileset/PNG/land_12.png" width="256" height="256"/>
+ </tile>
+ <tile id="125">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_3.png" width="121" height="114"/>
+ </tile>
+ <tile id="126">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_4.png" width="262" height="181"/>
+ </tile>
+ <tile id="127">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_5.png" width="181" height="136"/>
+ </tile>
+ <tile id="128">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_6.png" width="199" height="188"/>
+ </tile>
+ <tile id="129">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_7.png" width="125" height="100"/>
+ </tile>
+ <tile id="130">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_8.png" width="163" height="103"/>
+ </tile>
+ <tile id="131">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_9.png" width="148" height="101"/>
+ </tile>
+ <tile id="132">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_10.png" width="769" height="630"/>
+ </tile>
+ <tile id="133">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_1.png" width="64" height="64"/>
+ </tile>
+ <tile id="134">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_2.png" width="64" height="64"/>
+ </tile>
+ <tile id="135">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_3.png" width="64" height="64"/>
+ </tile>
+ <tile id="136">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_4.png" width="64" height="64"/>
+ </tile>
+ <tile id="137">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_5.png" width="64" height="64"/>
+ </tile>
+ <tile id="138">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_6.png" width="64" height="64"/>
+ </tile>
+ <tile id="139">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_7.png" width="64" height="64"/>
+ </tile>
+ <tile id="140">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_8.png" width="64" height="64"/>
+ </tile>
+ <tile id="141">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_9.png" width="64" height="64"/>
+ </tile>
+ <tile id="142">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_10.png" width="64" height="64"/>
+ </tile>
+ <tile id="143">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_11.png" width="64" height="64"/>
+ </tile>
+ <tile id="144">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_12.png" width="64" height="64"/>
+ </tile>
+ <tile id="145">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_13.png" width="64" height="64"/>
+ </tile>
+ <tile id="146">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_14.png" width="64" height="64"/>
+ </tile>
+ <tile id="147">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_15.png" width="64" height="64"/>
+ </tile>
+ <tile id="148">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_16.png" width="64" height="64"/>
+ </tile>
+ <tile id="149">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_17.png" width="64" height="64"/>
+ </tile>
+ <tile id="150">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_18.png" width="64" height="64"/>
+ </tile>
+ <tile id="151">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_19.png" width="64" height="64"/>
+ </tile>
+ <tile id="152">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_20.png" width="64" height="64"/>
+ </tile>
+ <tile id="153">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_21.png" width="64" height="64"/>
+ </tile>
+ <tile id="154">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_22.png" width="64" height="64"/>
+ </tile>
+ <tile id="155">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_23.png" width="64" height="64"/>
+ </tile>
+ <tile id="156">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_24.png" width="64" height="64"/>
+ </tile>
+ <tile id="157">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_25.png" width="64" height="64"/>
+ </tile>
+ <tile id="158">
+  <image source="free-2d-rpg-desert-tileset/PNG/road_26.png" width="64" height="64"/>
+ </tile>
+ <tile id="159">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_1.png" width="106" height="79"/>
+ </tile>
+ <tile id="160">
+  <image source="free-2d-rpg-desert-tileset/PNG/stones_2.png" width="114" height="104"/>
+ </tile>
+</tileset>
---
+++ demo/Assets/mapka-las/mapka_pustynia/spr_campfire_idle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="spr_campfire_idle" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="asety2/spr_campfire_idle.png" width="32" height="64"/>
+</tileset>
